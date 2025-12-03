a6d4aa4fa2ce0f58cc8bc96b44b227785689017f
NazarineWasonga <74545016+NazarineWasonga@users.noreply.github.com>
2025-12-03 18:48:18 +0300

    Add script to convert Keras model to TFLite format

---
+++ src/convert_to_tflite.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+# Load trained model
+model = tf.keras.models.load_model('../models/edge_model.h5')
+
+# Convert to TFLite
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save TFLite model
+with open('../models/edge_model.tflite', 'wb') as f:
+    f.write(tflite_model)
+
+print("TFLite model saved successfully!")
